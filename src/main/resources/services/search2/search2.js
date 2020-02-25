db226b42e9caa480b5126832920ccda7f0d3c26e
Tom-Anders Røst <trost@Tom-Anderss-MacBook-Pro.local>
2020-02-25 13:55:25 +0100

    IV-870, fix indentation

--- src/main/resources/services/search2/search2.js
+++ src/main/resources/services/search2/search2.js
@@ -1,43 +1,39 @@
-var searchUtils = require("/lib/search/searchUtils");
+var searchUtils = require('/lib/search/searchUtils');
 
 exports.get = handleGet;
 
 function handleGet(req) {
-  log.info("search2");
-  var params = req.params || {};
+    var params = req.params || {};
 
-  var model = {
-    word: false
-  };
+    var model = {
+        word: false,
+    };
 
-  if (!params.ord) {
-    params.ord = "";
-  }
-  if (params.ord.length > 200) {
-    params.ord = params.ord.substring(0, 200);
-  }
+    if (!params.ord) {
+        params.ord = '';
+    }
+    if (params.ord.length > 200) {
+        params.ord = params.ord.substring(0, 200);
+    }
 
-  var result = searchUtils.runInContext(
-    searchUtils.enonicSearchWithoutAggregations,
-    params
-  );
+    var result = searchUtils.runInContext(searchUtils.enonicSearchWithoutAggregations, params);
 
-  var c = params.c ? (!isNaN(Number(params.c)) ? Number(params.c) : 1) : 1;
-  var isMore = c * 20 < result.total;
-  var isSortDate = !params.s || params.s === "0";
+    var c = params.c ? (!isNaN(Number(params.c)) ? Number(params.c) : 1) : 1;
+    var isMore = c * 20 < result.total;
+    var isSortDate = !params.s || params.s === '0';
 
-  model = {
-    c: c,
-    isMore: isMore,
-    isSortDate: isSortDate,
-    s: params.s ? params.s : "0",
-    word: params.ord,
-    total: result.total.toString(10),
-    hits: result.hits
-  };
+    model = {
+        c: c,
+        isMore: isMore,
+        isSortDate: isSortDate,
+        s: params.s ? params.s : '0',
+        word: params.ord,
+        total: result.total.toString(10),
+        hits: result.hits,
+    };
 
-  return {
-    body: model,
-    contentType: "application/json"
-  };
+    return {
+        body: model,
+        contentType: 'application/json',
+    };
 }
